aa22126cf5a69dbbcf714fe6e76df1ea701af7bd
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-13 14:51:04 +0500

    Add files via upload
    Permitive and Reference datatype

---
+++ permitiveanddreferencedatatype/permitiveandreferencedatatype.js
@@ -0,0 +1,38 @@
+
+//decalare variable it is the example of the primitive data type 
+let num1 =6;
+let num2 =num1;
+
+//printting the value 
+console.log("the value of num1 is",num1);
+console.log("the value of num2 is",num2);
+//incrmenting the valuie 
+num1++;
+console.log("the value after the incremetiing the value ");
+//checking the vlaue after incrementiing 
+console.log("the value of num1 is",num1);
+console.log("the value of num2 is",num2);
+
+//declare variable of array and it is the example of the refernce data type 
+
+let array1=["item1","item2"];
+let array2 =array1;
+
+//printting the value
+console.log("value of array1",array1);
+console.log("value of array2",array2);
+
+//pushing method 
+array1.push("item3");
+
+console.log("the value after the applying the push method");
+
+//checking the value after the pushing value 
+console.log("the value of array1",array1);
+console.log("the value of array2",array2);
+
+//to check the value of array that is true or not 
+console.log(array1===array2)
+
+
+
