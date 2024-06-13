23ef4b966b0cf299b25e460e4387821944ba5c1c
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-13 14:52:29 +0500

    Add files via upload
    While loop

---
+++ whileloop/wileloop.js
@@ -0,0 +1,33 @@
+
+//declare variable 
+
+const fruits = ["banna","apple","grapes"];
+
+//applying while loop 
+let i =0;
+
+while(i<fruits.length){
+    //printting value 
+    console.log(fruits);
+    i++;
+}
+
+
+
+//declare variable and store in new varibale 
+
+const fruitss = ["banna","apple","grapes"];
+
+//makking new array
+const fruitss2 = [];
+
+//applying while loop 
+let j =0;
+
+while(j<fruits.length){
+    //adding push method and upper case 
+    fruitss2.push(fruitss[j].toUpperCase());
+    j++;
+}
+
+console.log(fruitss2);
