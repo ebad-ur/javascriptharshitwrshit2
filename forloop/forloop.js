a816d43bf3bcea186ccbfbd4ad9deb2a96705215
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-13 14:51:51 +0500

    Add files via upload
    For loop in Array

---
+++ forloop/forloop.js
@@ -0,0 +1,13 @@
+
+
+//declare variable 
+let fruits = ["apple","bananna","grapes","mango"];
+
+//cheking the length
+console.log(fruits.length);
+
+//applying for loop and if we want to do in upper case we will use upper case  
+
+for (let i=0; i<fruits.length; i++){
+    console.log(fruits[i].toUpperCase());
+}
