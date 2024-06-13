8cbf93b53b1c8f254ee76665e43149494025ae88
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-13 14:55:42 +0500

    Add files via upload
    Desruction in Array

---
+++ desruction/desctution.js
@@ -0,0 +1,9 @@
+//declare vzariable
+const myArray =["item1","item2","item3"]
+
+//declar5e new variabnle
+let [myvar1 , myvar2] = new Array
+
+//print 
+console.log("value of my var1",myvar1);
+console.log("value of my var2",myvar2)
