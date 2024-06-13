1540deaa18be4e966d9b731dc17f7da54c40b38d
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-13 15:00:17 +0500

    Add files via upload
    Object in side array

---
+++ objinside/objinside.js
@@ -0,0 +1,8 @@
+const users = [
+    {userId:1, firstname: "Ebad", gender:"male"},
+    {userId:2, firstname: "Shariq", gender:"female"},
+    {userId:23, firstname: "Thariq", gender:"female"},
+]
+for (let user of users){
+    console.log(user)
+}
