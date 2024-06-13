b214abb5dd8009128d247daa18f6bb7573475c77
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-13 14:50:32 +0500

    Add files via upload
    Simple method in Array

---
+++ simplemethod/simplemethod.js
@@ -0,0 +1,92 @@
+
+
+//push method is used to add the value in the array from the last 
+
+//declare variable 
+let fruits = ["apple","mango" , "grapes","papya"];
+//prinntting the value 
+console.log(fruits);
+
+//applying push method to add the vlaue from the last
+fruits.push ("banana");
+
+//checking that it is add or not 
+console.log(fruits)
+
+
+//pop method is yed to dleate the value in the  from the last 
+
+
+//declare variable 
+let fruit = ["apple","mango" , "grapes","papya"];
+//prinntting the value 
+console.log(fruit);
+
+//applying pop method
+fruit.pop();
+
+//checking that it is dleate or not 
+console.log(fruit)
+
+//chheking the value that pop has return 
+
+//declare variable 
+let fruitss = ["apple","mango" , "grapes","papya","banana"];
+//prinntting the value 
+console.log(fruitss);
+
+//using pop method by declaring in new variable 
+ let poppedfruits =fruitss.pop(); 
+
+//checking that it is dleate or not 
+console.log(fruitss);
+
+//chrchking return value 
+console.log("pop fruit is" ,poppedfruits);
+
+//unshift method is use to ad the value in array from the starting 
+
+//declare variable 
+let furt = ["aplle" ,"mosammi"];
+
+//prrinting the value 
+console.log(furt);
+
+//using unshift method to check the value is add from the start or not we can add multiple value at the time 
+furt.unshift("banana" ,"kenu","menu");
+
+
+//checking the value is print or nort 
+console.log(furt)
+
+//unshift is used to remove the variable from the starting of the array 
+
+
+//declare variable 
+let furts = ["jamun" ,"mosammi"];
+
+//prrinting the value 
+console.log(furts);
+
+//using the unshift method  we can use multiple time 
+furts.shift();
+furts.shift();
+//checking the value is dleate or nort 
+console.log(furts);
+
+
+//chheking the value that shift has return 
+
+//declare variable 
+let fruitsss = ["kiwi","mango" , "grapes","papya","banana"];
+//prinntting the value 
+console.log(fruitsss);
+
+//using pop method by declaring in new variable 
+ let poppedfruitss =fruitsss.shift(); 
+
+//checking that it is dleate or not 
+console.log(fruitsss);
+
+//chrchking return value 
+console.log("shift fruit is" ,poppedfruitss);
