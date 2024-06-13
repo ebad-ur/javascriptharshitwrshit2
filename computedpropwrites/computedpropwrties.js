00966bbb4956ab432944b5d9869d4f98f880011f
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-13 14:59:00 +0500

    Add files via upload
    Computed Properties

---
+++ computedpropwrites/computedpropwrties.js
@@ -0,0 +1,19 @@
+//declare the variable 
+
+const key1 = "ebad";
+const key2 = "hammad"
+
+//declare more value
+
+const value1 ="my value 1"
+const value2 = "my value 2"
+
+//now  we want in key 1 value and in key 2 value 2
+
+const obj= {}
+
+obj[key1]= "my value1"
+obj[key2]= "my value2"
+
+console.log(obj);
+
