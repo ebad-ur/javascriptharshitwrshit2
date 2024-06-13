b18fee7f9b99503cc3c55d102add8ef53c36a860
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-13 14:52:10 +0500

    Add files via upload
    Const in array

---
+++ constarray/constarray.js
@@ -0,0 +1,25 @@
+
+//declare variable 
+const fruits = ["Aplle","mango"];
+
+
+//applying push method 
+fruits.push ("bannana");
+
+//printting the value 
+console.log(fruits);
+
+
+
+//declare variable 
+let fruitss = ["Aplle","mango"];
+fruitss = []
+//applying push method 
+fruitss.push ("bannana");
+
+//printting the value 
+console.log(fruitss);
+
+//the differnce between the const and let makinf array is that if we call the same  array in let and the array
+//all the value which is stored inside the array whole value will be remove and the ans will come what we have 
+//push but when we do same thing with the const it will give the error 
