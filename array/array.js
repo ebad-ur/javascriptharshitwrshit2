486e4762129ec80cd5f51f73275c306948167a7a
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-13 14:48:49 +0500

    Add files via upload
    Array

---
+++ array/array.js
@@ -0,0 +1,32 @@
+
+//array is used to order the item is collection and we can store every value in array  
+//every aray is an object and we use method if the value come in true it is an array
+
+//declare the variable we write array in this barket[] 
+let fruits =["mango","apple","grapes","orange"];
+let number = [1,2,3,4];
+let mix = [1,1.2,"string" ,"null"]
+
+//printting the vlaues 
+console.log(fruits);
+console.log(number);
+console.log(mix);
+
+//findding the order value of array by ussing index and type as well 
+let array =["mango","apple","grapes","orange"]
+console.log(array [0]);
+
+//to check the value is this is array or not 
+console.log(typeof array);
+console.log(Array.isArray(fruits));
+
+//to change the element in the array the value will remove and give the vlaue which is updated will come \
+let fruit = ["mango","apple","grapes","orange"];
+//prrinting the value 
+console.log(fruit);
+
+//printting the value to check the update 
+fruit [1] = "bannna";
+console.log(fruit);
+
+
